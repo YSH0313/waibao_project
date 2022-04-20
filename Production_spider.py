a39cd6dcb93fe8b09658b92f42b984c06c255486
liuqingchuan <liuqingchuan@bailian.ai>
2022-04-20 18:53:59 +0800

    update

--- Production_spider.py
+++ Production_spider.py
@@ -1,5 +1,5 @@
 from spider_model import production
 
-production('zhaoxun_chongqingyuncai', False, 50, '袁少航', '重庆市政府采购云平台', 'ysh_spiders/chongqingyuncai/')
-#           爬虫名     是否增量        页码      所属人     备注      路径
+production('zhaoxun_chongqingyuncai', True, 50, '袁少航', '重庆市政府采购云平台', 'ysh_spiders/chongqingyuncai/')
+#           爬虫名     是否增量        每次更新页码      所属人     备注      路径
 # pruction('first_spider', False, 604800, '袁少航', redis=True)
